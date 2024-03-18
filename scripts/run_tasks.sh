ba50b723518e1214d0193c9c54e34b7ed7a91654
prasanthyss <prasanthyss1130@gmail.com>
2024-03-17 22:00:57 -0400

    Added experiments

--- scripts/run_tasks.sh
+++ scripts/run_tasks.sh
@@ -1,4 +1,10 @@
+# task-1
 python3 ./fine_tuning.py --model BERT-Base --dataset ANLI --n_epochs 20
 python3 ./fine_tuning.py --model BERT-Large --dataset ANLI --n_epochs 20
 python3 ./fine_tuning.py --model RoBERTa-Base --dataset ANLI --n_epochs 20
 python3 ./fine_tuning.py --model RoBERTa-Large --dataset ANLI --n_epochs 20
+
+# task-2
+python3 ./fine_tuning.py --model RoBERTa-Large --dataset MRPC --n_epochs 20
+python3 ./fine_tuning.py --model RoBERTa-Large --dataset QQP --n_epochs 20
+python3 ./fine_tuning.py --model RoBERTa-Large --dataset SST --n_epochs 20
