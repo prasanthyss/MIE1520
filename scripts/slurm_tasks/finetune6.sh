9ce0a4cdd5bd182e09c4b5317c848c6208199976
prasanthyss <prasanthyss1130@gmail.com>
2024-04-06 11:29:16 +0000

    logs

--- scripts/slurm_tasks/finetune6.sh
+++ scripts/slurm_tasks/finetune6.sh
@@ -5,5 +5,5 @@
 #SBATCH -w overture --gres=gpu:1
 
 # task-1
-python3 ./fine_tuning.py --model BERT-Base --lr 1e-6 --dataset QQP --n_epochs 6
+python3 ./fine_tuning.py --model BERT-Base --lr 1e-6 --eval_steps 10000 --dataset QQP --n_epochs 6
 python3 ./make_plot.py --file '../logs/finetune_bert-base-uncased_qqp.txt'
