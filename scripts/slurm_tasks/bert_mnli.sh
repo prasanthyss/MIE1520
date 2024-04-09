0df1c1cca2e3b913c5e9acc7eb52650a3523132d
prasanthyss <prasanthyss1130@gmail.com>
2024-04-09 11:13:24 +0000

    new logs

--- scripts/slurm_tasks/bert_mnli.sh
+++ scripts/slurm_tasks/bert_mnli.sh
@@ -5,5 +5,5 @@
 #SBATCH --mem=32G
 #SBATCH --gres=gpu:1
 
-python3 ./fine_tuning.py --model BERT-Base --dataset MNLI --n_epochs 6
+python3 ./fine_tuning.py --model BERT-Base --dataset MNLI --eval_steps 100000 --n_epochs 6
 python3 ./make_plot.py --file '../logs/finetune_bert-base-uncased_mnli.txt'
--- scripts/slurm_tasks/bert_yelp.sh
+++ scripts/slurm_tasks/bert_yelp.sh
@@ -5,5 +5,5 @@
 #SBATCH --mem=32G
 #SBATCH --gres=gpu:1
 
-python3 ./fine_tuning.py --model BERT-Base --dataset YELP --n_epochs 6
+python3 ./fine_tuning.py --model BERT-Base --dataset YELP --eval_steps 1000000 --n_epochs 6
 python3 ./make_plot.py --file '../logs/finetune_bert-base-uncased_yelp_review_full.txt'
