3ce685164a324e4b35c16d50bab7e4134563f5e6
prasanthyss <prasanthyss1130@gmail.com>
2024-04-18 00:53:40 +0000

    epochs

--- scripts/slurm_tasks/lora2.sh
+++ scripts/slurm_tasks/lora2.sh
@@ -2,10 +2,10 @@
 #SBATCH --job-name=bl_anli
 #SBATCH --nodes=1
 #SBATCH --mem=32G
-#SBATCH --gres=gpu:1
+#SBATCH -w overture --gres=gpu:1
 
 # ANLI
-python3 ./lora.py --model BERT-Large --dataset ANLI --n_epochs 6
+python3 ./lora.py --model BERT-Large --dataset ANLI --n_epochs 20
 
 # SST
 #python3 ./lora.py --model BERT-Large --dataset SST --n_epochs 6
--- scripts/slurm_tasks/lora3.sh
+++ scripts/slurm_tasks/lora3.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres=gpu:1
 
 # ANLI
-python3 ./lora.py --model RoBERTa-Base --dataset ANLI --n_epochs 6
+python3 ./lora.py --model RoBERTa-Base --dataset ANLI --n_epochs 20
 
 # SST
 #python3 ./lora.py --model RoBERTa-Base --dataset SST --n_epochs 6
